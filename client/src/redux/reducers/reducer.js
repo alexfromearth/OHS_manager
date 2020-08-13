ffb0f3c394c5ac1053cbf6890a7c6a36b950d6ab
Aleksandr <voronamk@mail.ru>
2020-08-13 17:42:06 +0300

    Fixed thunk for all employees

--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -3,7 +3,7 @@ import actionTypes from "../actionTypes/actionTypes";
 
 
 const initialState = {
-  companyId: null,
+  companyId: "5f35397ac776526b4ac12d94", // !!!!!!
   companyName: null,
   generalInfo: null,
   secret: null,
