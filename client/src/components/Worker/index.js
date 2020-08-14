68cf7fbd95981b57f958d27022f8c49da5dbacab
Bezz <bezobazov1999@gmail.com>
2020-08-14 14:40:15 +0300

    Worker end

--- client/src/components/Worker/index.js
+++ client/src/components/Worker/index.js
@@ -9,6 +9,11 @@ import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import ReplayIcon from '@material-ui/icons/Replay';
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 
+import Avatar from '@material-ui/core/Avatar';
+
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -20,7 +25,36 @@ const useStyles = makeStyles((theme) => ({
     alignItem: 'center'
   },
   btn: {
-    fontSize: 20
+    fontSize: 25
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+    marginRight: 30
+  },
+  info: {
+    marginLeft: '35%',
+    marginTop: '10%',
+    marginBottom: '15%',
+    fontSize: 30,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  infoRow: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  inbox: {
+    marginRight: 20
+  },
+  head:{
+    marginLeft: '33%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItem: 'center'
+  },
+  icon:{
+    marginRight: 5
   }
 }));
 
@@ -41,21 +75,52 @@ export default function Worker() {
     const fullname = worker.generalInfo.lastName + ' ' + worker.generalInfo.firstName + ' ' + worker.generalInfo.middleName
     console.log(fullname);
   }
-  //onClick={() => history.push('/employee/new')}
   console.log(worker);
   return (
     <>
-      <h1>{worker && (worker.generalInfo.lastName + ' ' + worker.generalInfo.firstName + ' ' + worker.generalInfo.middleName)}</h1>
-      <h1>{worker && worker.profInfo.profession}</h1>
+      <div className={classes.head}>
+        <Avatar
+          src={`https://img2.freepng.ru/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg`}
+          className={classes.large}
+        />
+        <h1>{worker && (worker.generalInfo.lastName + ' ' + worker.generalInfo.firstName + ' ' + worker.generalInfo.middleName)}</h1>
+      </div>
+      <Typography component="div" variant="body1" className={classes.info}>
+        <div className={classes.infoRow}>
+          <Box className={classes.inbox} color="text.primary">Дата рождения:</Box>
+          <Box color="info.main">20.01.20</Box>
+        </div>
+        <div className={classes.infoRow}>
+          <Box className={classes.inbox} color="text.primary">Место рождения:</Box>
+          <Box color="info.main">Тверь</Box>
+        </div>
+        <div className={classes.infoRow}>
+          <Box className={classes.inbox} color="text.primary">Место проживания:</Box>
+          <Box color="info.main">Москва</Box>
+        </div>
+        <div className={classes.infoRow}>
+          <Box className={classes.inbox} color="text.primary">Образование:</Box>
+          <Box color="info.main">Среднее специально</Box>
+        </div>
+        <div className={classes.infoRow}>
+          <Box className={classes.inbox} color="text.primary">Должность:</Box>
+          <Box color="info.main">Столяр</Box>
+        </div>
+        <div className={classes.infoRow}>
+          <Box className={classes.inbox} color="text.primary">Стаж работы:</Box>
+          <Box color="info.main">15 лет</Box>
+        </div>
+      </Typography>
+
       <div className={classes.root}>
-        <Button className={classes.btn} variant="contained" color="primary" onClick={() => history.push(`/employee/${id}/med`)} >
-          <LocalHospitalIcon /> Медицинский осмотр
+        <Button className={classes.btn} variant="contained" color="primary" onClick={() => history.push(`/employee/${id}/medicInfo`)} >
+          <LocalHospitalIcon className={classes.icon}/> Медицинский осмотр
         </Button>
-        <Button className={classes.btn} variant="contained" color="primary" >
-          <InsertDriveFileIcon /> Документы
+        <Button className={classes.btn} variant="contained" color="primary" onClick={() => history.push(`/employee/${id}/documents`)}>
+          <InsertDriveFileIcon className={classes.icon}/> Документы
         </Button>
-        <Button className={classes.btn} variant="contained" color="primary" >
-          <ReplayIcon /> Обновить информацию
+        <Button className={classes.btn} variant="contained" color="primary" onClick={() => history.push(`/employee/${id}/update`)}>
+          <ReplayIcon className={classes.icon}/> Обновить информацию
         </Button>
       </div>
     </>
