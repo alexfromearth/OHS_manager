806ab7ad284ff26adcb2b1f55297ba3eaf3c0208
Aleksandr <voronamk@mail.ru>
2020-08-15 18:19:00 +0300

    Small

--- client/src/components/ButtonList/Dashbord.js
+++ client/src/components/ButtonList/Dashbord.js
@@ -168,6 +168,7 @@ export default function Dashboard() {
                   <PrivateRoute exact path='/employees'> <Employees /> </PrivateRoute>
                   <PrivateRoute exact path='/employee/:id'> <Worker /> </PrivateRoute>
                   <PrivateRoute exact path='/employee/:id/medicInfo'> <MedicalExaminations /> </PrivateRoute>
+                  <PrivateRoute exact path='/employee/:id/documents'> <Dnd /> </PrivateRoute>
                 </Switch>
               </Grid>
             </Grid>
