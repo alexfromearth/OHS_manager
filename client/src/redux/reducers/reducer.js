2e0fde0c45b1f0b25e7a5d539deceb4b6c2833d6
Aleksandr <voronamk@mail.ru>
2020-08-17 16:00:56 +0300

    docs with dnd 1.0

--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -3,7 +3,7 @@ import actionTypes from "../actionTypes/actionTypes";
 
 
 const initialState = {
-  companyId: "5f3a6e3f43b428485d4456ca", // !!!!!!
+  companyId: "5f3a7228fb71834f0706d7ca", // !!!!!!
   companyName: null,
   generalInfo: null,
   secret: null,
