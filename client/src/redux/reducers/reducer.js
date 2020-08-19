7a940aca2d8478d69df9ef18b9ebdefc4ecdacb6
alexfromearth <shirokovaloff@gmail.com>
2020-08-19 11:16:36 +0300

    modal fixes

--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -3,7 +3,7 @@ import actionTypes from "../actionTypes/actionTypes";
 
 
 const initialState = {
-  companyId: "5f3bd38bc41434e902428f5a", // !!!!!!
+  companyId: "5f3cda207d907ac09cabfeaa", // !!!!!!
   companyName: null,
   generalInfo: null,
   secret: null,
